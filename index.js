2c3af1fadfb6b662ce591955fa8edbee15bc92b4
IP_SILALAHI <IP_SILALAHI@Admins-MacBook-Air.local>
2023-10-31 12:01:19 +0700

    initial commit

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ const startSock = () => {
         const msg = m. messages[0];
 
         if (!msg.key.fromMe && m.type === 'notify') {
-            console.log(msg);
+        //    console.log(msg);
             console.log ("No. HP : "+msg.key.remoteJid);
             console.log ("No. HP : "+msg.pushName);
-            console.log ("Pesan  : "+msg.message.conversation);
+        //    console.log ("Pesan  : "+msg.message.conversation);
             if (msg.key.remoteJid.includes('@s.whatsapp.net')) {
                 if (msg.message) {
                     if (msg.message.conversation == 'TagihanPJJ') {
---
+++ startup.cs
@@ -0,0 +1,10 @@
+app.UseSpa(spa =>
+    {
+        spa.Options.SourcePath = "ClientApp";
+
+        if (env.IsDevelopment())
+        {
+            spa.UseReactDevelopmentServer(npmScript: "start");
+        }
+    });
+    
